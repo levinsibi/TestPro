d895f785b30b39e38d8035d158a84369f2188e25
levinsibi <levinsibi@gmail.com>
2019-10-02 09:31:02 +0530

    b2 desktop

--- TestPro/src/testpackage/TestClass.java
+++ TestPro/src/testpackage/TestClass.java
@@ -15,7 +15,7 @@ public class TestClass {
 
 		System.out.println("Im B110 going to master!!!!");
 
-
+		System.out.println("Im B2 desktop");
 		System.out.println(44);
 		System.out.println(64);
 		System.out.println(84);
